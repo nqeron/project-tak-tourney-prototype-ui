0ea8cc15a197269c55b7ff040b47b1a7a7c372cf
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-15 00:24:43 -0500

    impl: basic reading info

--- routes.ts
+++ routes.ts
@@ -2,6 +2,13 @@ import { Router } from "jsr:@oak/oak/router";
 import type { RouterContext } from "jsr:@oak/oak/router";
 import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
 import { analyzeTournamentProgress } from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/tournament-analyzer.ts";
+import { isTournamentInfoFromJson } from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/types.guard.ts";
+import { isGameListResponse } from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/playtak-api/types.guard.ts";
+import { KNOWN_TOURNAMENTS } from "./data/data.ts";
+import {
+  TournamentInfo,
+  TournamentPlayer,
+} from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/types.ts";
 
 export const router = new Router();
 
@@ -15,19 +22,69 @@ function makeRenderer(templateName: string, templateOptions = {}) {
 
 router.get("/", makeRenderer("./simple", { name: "devp" }));
 
-router.get("/tournament", (ctx: RouterContext<string>) => {
-  const status = analyzeTournamentProgress({
-    tournamentInfo: {
-      players: [],
-      dateRange: {
-        start: new Date(),
-        end: new Date(),
-      },
+router.get("/tournaments", (ctx: RouterContext<string>) => {
+  return (makeRenderer("./tournaments", {
+    tournaments: Object.keys(KNOWN_TOURNAMENTS),
+  }))(ctx);
+});
+
+function parsePlayersCsv(playersCsv: string) {
+  const rows: [string, string][] = playersCsv.trim().split("\n").slice(1).map(
+    (line) => line.trim().split(","),
+  ).filter(Boolean).map(
+    (parts) => [parts[0], parts[1]] as [string, string],
+  );
+  return rows.map(([username, group]) => ({ username, group }));
+}
+
+router.get("/tournaments/:id", async (ctx: RouterContext<string>) => {
+  const id = ctx.params.id;
+  const tournamentData =
+    KNOWN_TOURNAMENTS[id as keyof typeof KNOWN_TOURNAMENTS] ?? null;
+  if (tournamentData === null) {
+    return ctx.response.status = 404;
+  }
+
+  const tournamentInfoFromJson = JSON.parse(
+    await Deno.readTextFile(tournamentData.infoPath),
+  );
+  if (!isTournamentInfoFromJson(tournamentInfoFromJson)) {
+    return ctx.response.status = 400;
+  }
+  const tournamentInfo: TournamentInfo = {
+    ...tournamentInfoFromJson,
+    dateRange: {
+      start: new Date(tournamentInfoFromJson.dateRange.start),
+      end: new Date(tournamentInfoFromJson.dateRange.end),
     },
-    games: [],
-  });
-  console.log(status);
+  };
+
+  let status = {};
+  if (
+    tournamentData.playersCsvUrl &&
+    tournamentData.gamesApiUrl
+  ) {
+    const gamesResponse = await (await fetch(tournamentData.gamesApiUrl))
+      .json();
+    if (!isGameListResponse(gamesResponse)) {
+      return ctx.response.status = 400;
+    }
+    const games = gamesResponse.items;
+
+    const playersCsv = await (await fetch(tournamentData.playersCsvUrl)).text();
+    const players: TournamentPlayer[] = parsePlayersCsv(playersCsv);
+    tournamentInfo.players = players;
+
+    status = analyzeTournamentProgress({
+      tournamentInfo,
+      games,
+    });
+  }
+
   return (makeRenderer("./tournament", {
+    tournament: {
+      name: tournamentInfo.name,
+    },
     status,
   }))(ctx);
 });
