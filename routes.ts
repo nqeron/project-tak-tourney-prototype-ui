914c4c44b125dfa3740cc3bd649b11cd50d9983a
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-15 23:44:40 -0500

    impl: simplify by using one api url with last few hundred games

--- routes.ts
+++ routes.ts
@@ -5,6 +5,7 @@ import { analyzeTournamentProgress } from "https://raw.githubusercontent.com/dev
 import { isTournamentInfoFromJson } from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/types.guard.ts";
 import { isGameListResponse } from "https://raw.githubusercontent.com/devp/project-tak-tourney-adhoc/refs/heads/main/src/playtak-api/types.guard.ts";
 import { API_URL, KNOWN_TOURNAMENTS } from "./data/data.ts";
+import { API_URL, KNOWN_TOURNAMENTS } from "./data/data.ts";
 import {
   TournamentInfo,
   TournamentPlayer,
