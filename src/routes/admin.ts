3e3a974c67b4a3576bbc5a244ee9f997753425c1
Dev Purkayastha <dev.purkayastha@gmail.com>
2025-01-26 02:09:20 -0500

    admin: UI to edit tournment as JSON

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,6 +2,8 @@ import { Router } from "jsr:@oak/oak/router";
 import { Status } from "jsr:@oak/oak";
 import type { RouterContext } from "jsr:@oak/oak/router";
 import { makeRenderer } from "../util/renderer.ts";
+import { KNOWN_TOURNAMENTS } from "../../data/data.ts";
+import { Tournament } from "../models/tournament.ts";
 
 const ADMIN_USERNAME = "admin";
 // Load this variable from local env for local development.
@@ -49,7 +51,46 @@ adminRouter.use(basicAuthMiddleware);
 
 // Example admin route
 adminRouter.get("/", (ctx: RouterContext<string>) => {
+  const tournaments = Object.keys(KNOWN_TOURNAMENTS).map((id) => ({ id }));
   return (makeRenderer("./admin/index.eta", {
     title: "Admin Dashboard",
+    tournaments,
   }))(ctx);
+});
+
+adminRouter.get("/tournament/:id", async (ctx: RouterContext<string>) => {
+  const kv = await Deno.openKv();
+  const id = ctx.params.id;
+  const tournament = await Tournament.load(id, kv);
+  return (makeRenderer("./admin/tournament.eta", {
+    title: "Admin Dashboard",
+    tournament,
+  }))(ctx);
+});
+
+adminRouter.post("/tournament/:id", async (ctx: RouterContext<string>) => {
+  const kv = await Deno.openKv();
+  const id = ctx.params.id;
+  const info = (await ctx.request.body.formData()).get("info");
+  if (!info) {
+    console.error("No data provided");
+    ctx.response.status = Status.BadRequest;
+    return;
+  }
+  let tournamentInfo: unknown;
+  try {
+    tournamentInfo = JSON.parse(info as string);
+  } catch (e) {
+    console.error("Invalid JSON", e);
+    ctx.response.status = Status.BadRequest;
+    ctx.response.body = "Invalid JSON";
+    return;
+  }
+  // Validation happens on save.
+  if (!(await Tournament.save(id, tournamentInfo, kv))) {
+    console.error("Failed to save tournament info");
+    ctx.response.status = Status.InternalServerError;
+    return;
+  }
+  ctx.response.redirect("/admin");
 });
